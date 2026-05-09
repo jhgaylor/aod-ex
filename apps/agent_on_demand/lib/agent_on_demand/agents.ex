00270fa429d09a6e975106b4a55ac11c2ddf8ef3
BinaryBourbon <jhgaylor+claudegithub@gmail.com>
2026-05-08 22:01:03 -0400

    feat: add dynamic filtering to Agents.list_agents/1

--- apps/agent_on_demand/lib/agent_on_demand/agents.ex
+++ apps/agent_on_demand/lib/agent_on_demand/agents.ex
@@ -6,10 +6,14 @@ defmodule AgentOnDemand.Agents do
   alias AgentOnDemand.Agents.Agent
   alias AgentOnDemand.Repo
 
-  def list_agents do
-    Repo.all(
-      from a in Agent, order_by: [desc: a.inserted_at, desc: a.id], preload: [:environment]
-    )
+  def list_agents(filters \\ []) do
+    from(a in Agent, order_by: [desc: a.inserted_at, desc: a.id], preload: [:environment])
+    |> apply_search(Keyword.get(filters, :search, ""))
+    |> apply_runtimes(Keyword.get(filters, :runtimes, []))
+    |> apply_env_ids(Keyword.get(filters, :env_ids, []))
+    |> apply_has_skills(Keyword.get(filters, :has_skills, false))
+    |> apply_has_mcp(Keyword.get(filters, :has_mcp, false))
+    |> Repo.all()
   end
 
   def get_agent(id), do: Repo.get(Agent, id) |> Repo.preload(:environment)
@@ -28,4 +32,47 @@ defmodule AgentOnDemand.Agents do
   end
 
   def delete_agent(%Agent{} = agent), do: Repo.delete(agent)
+
+  defp apply_search(query, ""), do: query
+
+  defp apply_search(query, search) do
+    term = "%#{search}%"
+    from a in query, where: like(a.name, ^term)
+  end
+
+  defp apply_runtimes(query, []), do: query
+
+  defp apply_runtimes(query, runtimes) do
+    from a in query, where: a.runtime in ^runtimes
+  end
+
+  defp apply_env_ids(query, []), do: query
+
+  defp apply_env_ids(query, env_ids) do
+    {none, real_ids} = Enum.split_with(env_ids, &(&1 == "none"))
+
+    cond do
+      none != [] and real_ids != [] ->
+        from a in query,
+          where: is_nil(a.environment_id) or a.environment_id in ^real_ids
+
+      none != [] ->
+        from a in query, where: is_nil(a.environment_id)
+
+      true ->
+        from a in query, where: a.environment_id in ^real_ids
+    end
+  end
+
+  defp apply_has_skills(query, false), do: query
+
+  defp apply_has_skills(query, true) do
+    from a in query, where: fragment("json_array_length(?)", a.skills) > 0
+  end
+
+  defp apply_has_mcp(query, false), do: query
+
+  defp apply_has_mcp(query, true) do
+    from a in query, where: fragment("? != '{}'", a.mcp_servers)
+  end
 end
